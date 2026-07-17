4f8520b667bcf5203707bd5a21110af7ae86f262
samarth <samarthjain@thoughtwin.com>
2026-07-17 03:48:51 -0700

    Sign IN and Sig up forms Part is complete

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,32 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./Header";
 
 const Login = () => {
+  const [isSignUpForm, updateSignUpForm] = useState(true);
+  const toggleForm = () => {
+    updateSignUpForm(!isSignUpForm);
+  };
   return (
     <div className="relative">
       <Header />
-      <div className="absolute">
+      <div>
         <img
+          className="w-full h-screen object-cover"
           src="https://assets.nflxext.com/ffe/siteui/vlv3/ffa9d590-69c5-406f-bff9-e2ced3baa6ad/web/IN-en-20260713-TRIFECTA-perspective_75c0557e-9bbb-4149-9913-b87d4d7a30b7_large.jpg"
           alt="background"
         />
       </div>
-      <form className="absolute mx-80 my-36 w-3/12 bg-black right-0 left-0 p-12 text-white rounded-lg bg-opacity-50">
-        <h1 className="font-bold text-3xl py-4">Sign In </h1>
+
+      <form className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-3/12 bg-black/50 p-12 text-white rounded-lg">
+        <h1 className="font-bold text-3xl py-4">
+          {isSignUpForm ? "Sign In" : "Sign Up"}
+        </h1>
+        {!isSignUpForm && (
+          <input
+            type="text"
+            placeholder="Name"
+            className="bg-gray-700 w-full p-4 my-4 rounded"
+          />
+        )}
         <input
           type="text"
           placeholder="Email Address"
-          className=" bg-gray-700 w-full p-4 my-4"
+          className="bg-gray-700 w-full p-4 my-4 rounded"
         />
+
         <input
           type="password"
-          placeholder="password"
-          className=" bg-gray-700 w-full p-4 my-4"
+          placeholder="Password"
+          className="bg-gray-700 w-full p-4 my-4 rounded"
         />
+
         <button className="p-4 my-6 bg-red-700 w-full rounded-lg">
-          Sign In
+          {isSignUpForm ? "Sign In" : "Sign Up"}
         </button>
-        <p className="py-4">New to NetFlix ? Sign Up Now</p>
+
+        <p className="py-4" onClick={toggleForm}>
+          {isSignUpForm
+            ? "New to Netflix? Sign Up Now"
+            : "Already Registreresd"}
+        </p>
       </form>
     </div>
   );
